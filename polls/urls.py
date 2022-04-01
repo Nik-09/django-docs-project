155826ed0ae304bd1ba4a354f86a1769ab7f71fc
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-04-01 23:15:07 +0530

    Part: 3 from documentation.

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='datail'),
+    path('<int:question_id>/result', views.result, name='result'),
+    path('<int:question_id>/vote', views.vote, name="vote"),
 ]
